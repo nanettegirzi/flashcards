823c21a0992d988c0c41882d54e8edd77edb8492
Faiza Husain and Nanette Girzi <nanette@>
2018-01-09 16:48:55 -0800

    add toggle in js

---
+++ js/scripts.js
@@ -0,0 +1,97 @@
+$(document).ready(function (){
+  $(".clickable1").click(function (){
+    $(".js").toggle();
+    $(".def1").toggle();
+  });
+
+  $(".clickable2").click(function (){
+    $(".op").toggle();
+    $(".def2").toggle();
+  });
+
+  $(".clickable3").click(function (){
+    $(".var").toggle();
+    $(".def3").toggle();
+  });
+
+  $(".clickable4").click(function (){
+    $(".vnc").toggle();
+    $(".def4").toggle();
+  });
+
+  $(".clickable5").click(function (){
+    $(".func").toggle();
+    $(".def5").toggle();
+  });
+
+  $(".clickable6").click(function (){
+    $(".meth").toggle();
+    $(".def6").toggle();
+  });
+
+  $(".clickable7").click(function (){
+    $("arg").toggle();
+    $(".def7").toggle();
+  });
+
+  $(".clickable8").click(function (){
+    $(".para").toggle();
+    $(".def8").toggle();
+  });
+
+  $(".clickable9").click(function (){
+    $(".ret").toggle();
+    $(".def9").toggle();
+  });
+
+  $(".clickable10").click(function (){
+    $(".cm").toggle();
+    $(".def10").toggle();
+  });
+
+  $(".clickable11").click(function (){
+    $(".str").toggle();
+    $(".def11").toggle();
+  });
+
+  $(".clickable12").click(function (){
+    $(".boo").toggle();
+    $(".def12").toggle();
+  });
+
+  $(".clickable13").click(function (){
+    $(".undef").toggle();
+    $(".def13").toggle();
+  });
+
+  $(".clickable14").click(function (){
+    $(".nan").toggle();
+    $(".def14").toggle();
+  });
+
+  $(".clickable15").click(function (){
+    $(".esc").toggle();
+    $(".def15").toggle();
+  });
+
+  $(".clickable16").click(function (){
+    $(".alr").toggle();
+    $(".def16").toggle();
+  });
+
+  $(".clickable17").click(function (){
+    $(".com").toggle();
+    $(".def17").toggle();
+  });
+
+  $(".clickable18").click(function (){
+    $(".jq").toggle();
+    $(".def18").toggle();
+  });
+
+  $(".clickable19").click(function (){
+    $(".attr").toggle();
+    $(".def19").toggle();
+  });
+
+});
